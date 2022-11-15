8fef36c7ca963ebdaa59f2620b1c4932c4065cbb
phlc <pedrocarvalho@outlook.com>
2022-11-15 13:00:28 -0300

    gui paths redraw; tomasulo.py states started

--- tomasulo.py
+++ tomasulo.py
@@ -1,14 +1,135 @@
 
+class State:
+    def __init__(self):
+        #control
+        self.clock = 0
+        self.pc = 0
+
+        #instruction cache
+        self.instruction_cache = []
+
+        #data cache
+        self.data_cache = []
+
+        #instruction queue
+        self.instruction_queue = {
+            "queue": [],
+            "size": 0
+        }
+
+        #reorder buffer
+        self.reorder_buffer = {
+            "buffer": [],
+            "size": 0
+        }
+
+        #registers
+        self.registers = {
+            "r0": True,
+            "r1": True,
+            "r2": True,
+            "r3": True,
+            "r4": True,
+            "r5": True,
+            "r6": True,
+            "r7": True,
+            "r8": True,
+            "r9": True,
+        }
+
+
+        #Reservation Stations
+        self.reservation_branch = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_mult1 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_mult2 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_add1 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_add2 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_add3 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_load1 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+        self.reservation_load2 = {
+            "id": True,
+            "busy": True,
+            "op": True,
+            "vj": True,
+            "vk": True,
+            "qj": True,
+            "qk": True,
+            "a": True
+        }
+
+        
+
 # Global Variables
 
-global clock, pc, inst_cache, data_cache
-clock = 0
-pc = 0
+global states
+states = []
 
 
 def load(fname):
-    global inst_cache
-    global data_cache
+    global estates
     with open(fname, "r") as file:
         line = file.readline().strip('\n').strip().lower()
         while(line != ".data"):
