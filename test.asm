8ffa4cffbf3144f4d84f3cc7d0382a5a31605d07
phlc <pedrocarvalho@outlook.com>
2022-12-05 10:53:14 -0300

    new test.asm

--- test.asm
+++ test.asm
@@ -1,10 +1,12 @@
-LW r1, 0(r0)
+LW r1, 12(r0)
 ADDI r2, r0, 2
-SW r2, 0(r0)
+SW r2, 0(r1)
 LW r3, 4(r1)
 BEQ r3, r2, 16
 ADD r2, r3, r1
-MUL r4, r3, r2
+ADDI r8, r1, 0
+MUL r3, r1, r2
+BEQ r1, r8, 8
 DIV r8, r3, r4
 ADDI r2, r2, 8
 ECALL
